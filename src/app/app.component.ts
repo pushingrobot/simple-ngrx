3edc73854fe8850a9a9b57f138d7293b62c1c873
David Stephens <pushingrobot@me.com>
2017-09-09 11:47:09 -0600

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sn-root',
+  template: `
+    <p>
+      app Works!
+    </p>
+  `,
+  styles: []
+})
+export class AppComponent {
+  title = 'sn';
+}
