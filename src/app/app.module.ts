1d75aa7fcd84d5f4830efab00839d2f8fdcb4673
David Stephens <pushingrobot@me.com>
2017-09-09 14:32:27 -0600

    Add Chrome DevTools support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { counterReducer } from './reducers/counter.reducer';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -14,6 +15,9 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     StoreModule.forRoot({ counter: counterReducer }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25 //  Retains last 25 states
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
